c8caf0c8587546ffe817a8adf824a9d223c61bbe
kaaang <kaaang@kakao.com>
2022-11-04 20:06:40 +0900

    move

--- src/main/java/com/test/branch/BranchApplication.java
+++ src/main/java/com/test/branch/BranchApplication.java
@@ -9,6 +9,7 @@ public class BranchApplication {
     public static void main(String[] args) {
         SpringApplication.run(BranchApplication.class, args);
         System.out.println("1");
+        System.out.println("move");
     }
 
 }
